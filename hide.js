06c0f465cea092c35c9deb3a95898075d07d4c3a
Sp4nch <staskiselevv@outlook.com>
2023-10-16 06:05:38 +0300

    add header

---
+++ hide.js
@@ -0,0 +1,4 @@
+document.getElementById('burger').onclick = function () {
+    document.getElementById('burg-ac').classList.toggle('links-active');
+    document.getElementById('burger').classList.toggle('burg-active');
+  };
