7a5cb7a8c08e1807a46daee11f512d4293c68b5c
Sp4nch <staskiselevv@outlook.com>
2023-10-24 01:31:01 +0300

    upd

--- hide.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('burger').onclick = function () {
-    document.getElementById('burg-ac').classList.toggle('links-active');
-    document.getElementById('burger').classList.toggle('burg-active');
-  };
