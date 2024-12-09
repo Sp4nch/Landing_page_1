1bc9dce66050657cc714297cd6adadcbd605556e
stas <staskiselevv@outlook.com>
2024-12-09 18:07:01 +0300

    new project

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const btnBurger = document.querySelector('.burgher-menu');
-const navigation = document.querySelector('.horizantal-menu')
-btnBurger.addEventListener('click', () => {
-    btnBurger.classList.toggle('brg-active')
-    navigation.classList.toggle('hrz-active')
-})
